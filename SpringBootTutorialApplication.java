02e333c5191a68d71547c80dadf71f7f49cbf0bb
sevvalavag <sevalavag2001@hotmail.com>
2022-07-09 15:07:26 +0300

    Create SpringBootTutorialApplication.java
    Sprin boot dosyası eklendi

---
+++ SpringBootTutorialApplication.java
@@ -0,0 +1,33 @@
+package com.example.springboottutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTutorialApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value= "myName", defaultValue = "World") String name){
+
+         Oyuncu[] oyuncular = DosyaIsleyici.oyuncularıOku();
+
+        String isimler = "" ;
+
+        for( int i = 0 ; i< oyuncular.length ; i++ ){
+
+            isimler = isimler + oyuncular[i].isim + "\n";
+        }
+
+        return isimler;
+
+    }
+
+}
